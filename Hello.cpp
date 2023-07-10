ab75a4951232c3b1afd686ed7308aa18ebabf594
Supawich Peantanyakorn <134713409+savevenjer@users.noreply.github.com>
2023-07-10 10:38:11 +0700

    Add files via upload

---
+++ Hello.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{   cout<<"Student Code : 66060216621178"<<endl;
+	cout<<"Student Name : Supawich Peantanyakorn"<<endl;
+	cout<<"    Division : Information Technology"<<endl;
+	cout<<"        Year : 1"<<endl;
+	cout<<"        Room : RB"<<endl;
+	cout<<"         Sex : Male"<<endl;
+	cout<<"         Age : 18"<<endl;
+   system("pause") ;
+	return (0) ;
+}
