f31d99b4f5a939f525a28494a11422bb8b7c95c6
Supawich Peantanyakorn <134713409+savevenjer@users.noreply.github.com>
2023-09-18 11:22:32 +0700

    Add files via upload

--- Function1.cpp
+++ Function1.cpp
@@ -1,51 +1,38 @@
 #include <iostream>
+#include <string>
 using namespace std;
-void CalCircle();
-void Calrec();
-
 int main()
-{	char ch;
-	do{ 
-		cout<<"Program Calculate of Circle."<<endl;
-		cout<<"****************************"<<endl;
-		cout<<"1.Circle"<<endl;
-		cout<<"2.Rectangle"<<endl;
-		cout<<"3.Exit"<<endl;
-		cout<<"Choose Menu :";
-		cin>>ch;
-		if(ch == '1') CalCircle();
-			else if(ch == '2') Calrec();
-				else if(ch == '3') cout<<"Exit"<<endl;
-				else cout<<"wrong Menu"<<endl;
-
+{	
+	int num[4];
+	int total=0;
+	int min=999;
+	int max=0;
+	string name[4];
+	for(int i=0;i<4;i++)
+	{	cout<<"Enter Name["<< i<<"] :";
+		cin>>name[i];
+		cout<<"Enter Score["<< i <<"] :";
+		cin>>num[i];
 		
-	}while(ch != 3);
+	}
+	
+	cout<<"-----------------------\n";
+	cout<<"Name            Score\n";
+	cout<<"-----------------------\n";
+	for(int i=0; i<4;i++)
+	{
+		cout<<name[i]<<"\t\t"<<num[i]<<endl;
+		total = total+num[i];
+		if(num[i]>max)
+			max = num[i];
+		else if(num[i]<min)
+			min = num[i];
+	}
+		cout<<"-----------------------\n";
+		cout<<"Max Score"<<max<<endl;
+		cout<<"Min Score"<<min<<endl;
+		cout<<"Total = "<<total<<endl;
+		cout<<"Average = "<<(float)total/4<<endl;
 	system("pause");
-	return(0);
+	return 0;
 }
-
-void CalCircle()
-{
-	float Radius,Area,Circumference;
-	cout<<"input radius :";
-	cin>>Radius;
-	Area = 3.14f*Radius*Radius;
-	Circumference = 2*3.14f*Radius;
-	cout<<endl;
-	cout<<"Area of circle :"<<Area<<endl;
-	cout<<"Circumference of circle :"<<Circumference<<endl;
-	cout<<"Diameter of circle :"<<(2*Radius)<<endl;
-}
-
-void Calrec()
-{
-	float H,L,sum;
-	cout<<"in put width"<<endl;
-	cin>>H;
-	cout<<"in put length"<<endl;
-	cin>>L;
-	sum = H*L;
-	cout<<endl;
-	cout<<"Area of Rectangle : "<<sum<<endl;
-}
-
